7410148b2875eca6b53eda14d2d6019f28b3aabb
lambchops9 <kansarashashwat@gmail.com>
2025-03-16 23:08:27 +0700

    Create inverted.java

---
+++ inverted.java
@@ -0,0 +1,18 @@
+public class stars {
+    public static void main(String[]args) {
+        int N= 5;
+        for (int i = 1; i<=N;i++) {
+            for (int j = N; j>=i ;j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// output
+// *****
+//****
+//***
+//**
+//*
