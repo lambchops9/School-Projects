73ccc5236f3e6ce3ead4d527d414fb1b5b4ec98e
lambchops9 <kansarashashwat@gmail.com>
2025-03-16 23:20:23 +0700

    Create fiveStars.java

---
+++ fiveStars.java
@@ -0,0 +1,11 @@
+public class fiveStars {
+   public static void main(String[]args) {
+       int N = 5;
+       for (int i = 1; i <= N; i++) {
+           for (int j = 1; j <=5 ;j++);
+           System.out.println("*");
+       }
+   }
+}
+
+// Output is wrong 
